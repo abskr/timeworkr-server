3cb718084b4762a44c1d44ddbe2c785aeaf150f5
ABskr <bskr26@gmx.de>
2021-09-17 01:12:03 +0200

    re-model

--- src/apis/row/rowController.js
+++ src/apis/row/rowController.js
@@ -18,15 +18,20 @@ export const createRow = async(req, res) => {
       });
     }
     
-    console.log(req.body)
     const { date, startTime, endTime, pause, plannedTime, rate, task } = req.body
 
     let newRow = new RowModel({
       sheetId,
       userId,
       date,
-      startTime,
-      endTime,
+      startTime: {
+        hour: startTime.hour,
+        minute: startTime.minute,
+      },
+      endTime: {
+        hour: endTime.hour,
+        minute: endTime.minute,
+      },
       pause,
       plannedTime: {
         hours: plannedTime.hours,
@@ -34,25 +39,20 @@ export const createRow = async(req, res) => {
       },
       rate: {
         per: rate.per,
-        amount: rate.amount
+        amount: rate.amount,
       },
-      task
+      task,
     });
+    console.log(newRow)
     const row = await newRow.save()
-    const updateSheet = await SheetModel.updateSheetSum(sheetId, userId)
     res.json({
-      created : {
-        rowId : row._id
-      },
-      updated: {
-        sheetId : updateSheet._id
-      }
+        row
     })
 } catch (error) {
     console.log(error)
     res.status(500).json({
-      error
-    })
+      error: 'Generic server error!',
+    }); 
   }
 }
 
@@ -123,8 +123,14 @@ export const editRow = async(req, res) => {
       { _id: rowId, sheetId, userId },
       {
         date,
-        startTime,
-        endTime,
+        startTime: {
+          hour: startTime.hour,
+          minute: startTime.minute,
+        },
+        endTime: {
+          hour: endTime.hour,
+          minute: endTime.minute,
+        },
         pause,
         plannedTime: {
           hours: plannedTime.hours,
@@ -132,13 +138,13 @@ export const editRow = async(req, res) => {
         },
         rate: {
           per: rate.per,
-          amount: rate.amount
+          amount: rate.amount,
         },
         order: {
           from: order.from,
           task: order.task,
         },
-        task
+        task,
       },
       { new: true, omitUndefined: true }
     );
@@ -149,13 +155,8 @@ export const editRow = async(req, res) => {
       })
     }
 
-    const updateSheet = await SheetModel.updateSheetSum(sheetId);
-
     res.json({
-      updated: {
-        rowId: row._id,
-        sheetId: updateSheet._id
-      }
+      row
     })
   } catch (error) {
     console.log(error)
@@ -189,119 +190,14 @@ export const deleteRow = async(req, res) => {
         error: 'Row not found!',
       });
     }
-        
-    const updateSheet = await SheetModel.updateSheetSum(sheetId);
     
     res.json({
-      deleted: {
-        rowId: row._id
-      },
-      updated: {
-        sheetId: updateSheet._id
-      }
+     row
     })
   } catch (error) {
      console.log(error);
      res.status(500).json({
        error: 'Generic server error!',
      });
-  }
-}
-
-export const createRowMock = async(req, res) => {
-      // const {
-      //   date,
-      //   startTime,
-      //   endTime,
-      //   pause,
-      //   plannedTime,
-      //   hourlyRate,
-      //   order,
-      // } = req.body;
-  const inputArr = [
-    {
-      sheetId: mongoose.Types.ObjectId('612f97f03bbb3840d87b1530'),
-      userId: mongoose.Types.ObjectId('612cafabf4b34e7aa45b1a49'),
-      date: '2021-09-01',
-      startTime: '08:00',
-      endTime: '16:00',
-      pause: 30,
-      plannedTime: {
-        hours: 8,
-      },
-      rate: {
-        per: 'hour',
-        amount: 11.5,
-      },
-    },
-    {
-      sheetId: mongoose.Types.ObjectId('612f97f03bbb3840d87b1530'),
-      userId: mongoose.Types.ObjectId('612cafabf4b34e7aa45b1a49'),
-      date: '2021-09-02',
-      startTime: '08:00',
-      endTime: '16:00',
-      pause: 30,
-      plannedTime: {
-        hours: 8,
-      },
-      rate: {
-        per: 'hour',
-        amount: 11.5,
-      },
-    },
-    {
-      sheetId: mongoose.Types.ObjectId('612f97f03bbb3840d87b1530'),
-      userId: mongoose.Types.ObjectId('612cafabf4b34e7aa45b1a49'),
-      date: '2021-09-03',
-      startTime: '13:00',
-      endTime: '19:00',
-      pause: 0,
-      plannedTime: {
-        hours: 5,
-      },
-      rate: {
-        per: 'hour',
-        amount: 14,
-      },
-    },
-    {
-      sheetId: mongoose.Types.ObjectId('612f97f03bbb3840d87b1530'),
-      userId: mongoose.Types.ObjectId('612cafabf4b34e7aa45b1a49'),
-      date: '2021-09-06',
-      startTime: '13:00',
-      endTime: '19:00',
-      pause: 0,
-      plannedTime: {
-        hours: 6,
-      },
-      rate: {
-        per: 'hour',
-        amount: 14,
-      },
-    },
-    {
-      sheetId: mongoose.Types.ObjectId('612f97f03bbb3840d87b1530'),
-      userId: mongoose.Types.ObjectId('612cafabf4b34e7aa45b1a49'),
-      date: '2021-09-07',
-      startTime: '18:00',
-      endTime: '22:00',
-      pause: 0,
-      plannedTime: {
-        hours: 4,
-      },
-      rate: {
-        per: 'hour',
-        amount: 15,
-      },
-    },
-  ];
-  try {
-    const rows = await RowModel.insertMany(inputArr)
-    res.json(rows)
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: 'Generic server error!',
-    });
   }
 }
