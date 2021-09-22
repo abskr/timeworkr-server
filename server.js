36bfd4ee18de577f07d972e2697a80446cf07014
ABskr <bskr26@gmx.de>
2021-09-22 13:49:02 +0200

    mod mongoose connect

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ const server = express()
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
     useUnifiedTopology: true
   })
   .then(() => {
